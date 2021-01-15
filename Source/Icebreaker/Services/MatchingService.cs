d03749001a1b7b9c019f18984ae28bdfd87ec63c
Alison Fang <alisonfang16@gmail.com>
2021-01-15 12:10:31 -0500

    put more stuff back

--- Source/Icebreaker/Services/MatchingService.cs
+++ Source/Icebreaker/Services/MatchingService.cs
@@ -195,7 +195,6 @@ namespace Icebreaker.Services
             if (users.Count > 1)
             {
                 this.telemetryClient.TrackTrace($"Making {users.Count / 2} pairs among {users.Count} users");
-                this.telemetryClient.TrackTrace(users[0]);
             }
             else
             {
